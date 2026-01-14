20e4867610f1de8108a04d46f7a6165e343c7066
Daniel Garza <dgarz160@stu.southtexascollege.edu>
2026-01-14 14:30:52 -0600

    test

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'fb7bb23f03b6994dafc674c074d01761'; // Replace with your actual TMDb API key
+    const BASE_URL = 'https://api.themoviedb.org/3';
+    const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
